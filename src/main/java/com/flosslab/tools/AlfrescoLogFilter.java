9e9b62c6e99839585405899a8fbbfa8b1fed7ac4
sardylan <sardylan@gmail.com>
2018-08-30 10:56:27 +0200

    Adds license

--- src/main/java/com/flosslab/tools/AlfrescoLogFilter.java
+++ src/main/java/com/flosslab/tools/AlfrescoLogFilter.java
@@ -1,3 +1,23 @@
+/*
+
+Alfresco Log Filter
+Copyright (C) 2018  Flosslab
+- Luca Cireddu (luca.cireddu@flosslab.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+ */
+
 package com.flosslab.tools;
 
 
@@ -6,6 +26,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 public class AlfrescoLogFilter implements Filter {
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
 
